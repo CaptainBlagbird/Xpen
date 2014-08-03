f6a9c915bc8d663992230f22f5daebfedd5d087b
skynet1 <thecrazymancan@gmail.com>
2014-08-03 17:18:23 -0500

    Modified Xpen to allow you to switch to uppercase by rotating around the whole circle.

--- src/com/captainblagbird/xpen/XpenView.java
+++ src/com/captainblagbird/xpen/XpenView.java
@@ -29,7 +29,6 @@ public class XpenView extends View
 							{'m', 'b', 'c', 'l'},
 							{'f', 'p', 'z', 'k'},
 							{'ä', 'q', '.', 'ö'}};	// Table of the arranged characters
-	
 	public XpenView(Context context, AttributeSet attrs)
 	{
 		super(context,attrs);
@@ -233,20 +232,11 @@ public class XpenView extends View
 			outsideOfCircle = false;
 	
 			// Check if dir is valid (crossed 1 ... 4 lines)
-			if ((dir != 0) && (dir <= 4) && (dir >= -4))
+			if ((dir != 0))
 			{
 				// Calculate array index i with dir
-				int i;
-				if (dir > 0)
-				{
-					// dir = 1 ... 4  to  i= 0 ... 3
-					i = dir - 1;
-				}
-				else  // dir < 0
-				{
-					// dir = -1 ...-4  to  i= 4 ... 7
-					i = 3 - dir;
-				}
+				int i=(dir<0) ? 3+(Math.abs(dir)%4) : (Math.abs(dir)%4)-1; // This uses a neat math trick called "numeration" AKA "clock math"
+				// This also takes advantage of an obscure operator known as the "ternary conditional"
 				
 				// Send character in correct case
 				if(uppercase)
@@ -276,13 +266,15 @@ public class XpenView extends View
 				// (it's possible to "uncross" a line by going back in the other direction, only the final value is used)
 				if ((ac > Math.PI / 4) && (al <= Math.PI / 4)) dir++;  // Crossed line CCW
 				if ((ac <= Math.PI / 4) && (al > Math.PI / 4)) dir--;  // Crossed line CW
+				if (Math.abs(dir)%4==1 && Math.abs(dir)>4 && uppercase) uppercase=false; // Has gone in a full circle and is already uppercase
+				else if (Math.abs(dir)%4==1 && Math.abs(dir)>4 && !uppercase) uppercase=true; // Has gone in a full circle and is not uppercase
 			}
 		}
 	
 		// Remember point
 		posLast = posNow;
 	}
-	
+
 	private void img_Input_onTouchUp(MotionEvent e)
 	{
 		// Get touch point
